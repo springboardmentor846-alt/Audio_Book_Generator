50c75741ba071a20addf9fee0189465482d4ddab
Swati Singh <swatisingh.it2023@nsec.ac.in>
2026-03-23 19:48:27 +0530

    deleted listenfile

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -290,7 +290,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Upload from "./pages/Upload";
 import StudyMode from "./pages/StudyMode";
-import ListenerMode from "./pages/ListenerMode";
+
 import Insights from "./pages/Insights";
 import StudyInsights from "./pages/StudyInsights";
 
